430e2a28bfbe06f663f2902d346bc262f2b5e432
ayld <iliev.simeon@gmail.com>
2012-08-15 11:56:37 +0300

    - added assembler interface

---
+++ src/main/java/net/ayld/core/service/Assembler.java
@@ -0,0 +1,23 @@
+package net.ayld.core.service;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+import net.ayld.core.dto.Dto;
+import net.ayld.core.domain.Entity;
+
+public interface Assembler<D extends Dto<I>, E extends Entity<I>, I extends Serializable> extends Serializable{
+	
+	public D toDto(E entity);
+	
+	public D toEntity(D Dto);
+	
+	public List<D> toDtos(List<E> entities);
+	
+	public List<E> toEntities(List<D> dtos);
+	
+	public Set<D> toDtos(Set<E> entities);
+	
+	public Set<E> toEntities(Set<D> dtos);
+}
